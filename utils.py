062e29d2f63437af7a1a3743231490d442e8463d
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-21 18:34:55 +0900

    feat: seed_everything

---
+++ utils.py
@@ -0,0 +1,14 @@
+import random, os
+import numpy as np
+import torch
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.use_deterministic_algorithms(True)
+    torch.backends.cudnn.benchmark = True
+    
