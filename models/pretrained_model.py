21a9bec130a98dd5c6ff9e8d0e0203506884fb84
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-28 02:16:30 +0900

    fix: ignore_mismatched_sizes=True to change classifier of the model

--- models/pretrained_model.py
+++ models/pretrained_model.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForAudioClassification, AutoFeatureExtractor
 class HuggingFace(nn.Module):
     def __init__(self, args, input_size, output_size) -> None:
         super().__init__()
-        self.model = AutoModelForAudioClassification.from_pretrained(args.pretrained_model, num_labels=output_size)
+        self.model = AutoModelForAudioClassification.from_pretrained(args.pretrained_model, num_labels=output_size, ignore_mismatched_sizes=True)
 
     def forward(self, x):
         return self.model(x).logits
