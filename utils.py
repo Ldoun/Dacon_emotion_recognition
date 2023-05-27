e0dab9ac5509062d5bbd183462fa25afa6783920
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-28 00:50:28 +0900

    fix: incompatible with huggingface model

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def seed_everything(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    torch.use_deterministic_algorithms(True)
+    #os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    #torch.use_deterministic_algorithms(True)
     torch.backends.cudnn.benchmark = True
     
