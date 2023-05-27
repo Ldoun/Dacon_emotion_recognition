76de9b9fed0a49b448f78dd210c9fe54452b1cf6
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-28 00:49:43 +0900

    fix: return logits of hugging face models

--- models/pretrained_model.py
+++ models/pretrained_model.py
@@ -4,9 +4,8 @@ from transformers import AutoModelForAudioClassification, Wav2Vec2FeatureExtract
 class Wav2Vec2(nn.Module):
     def __init__(self, args, input_size, output_size) -> None:
         super().__init__()
-        self.model_name = args.pretrained_model
-        self.model = AutoModelForAudioClassification.from_pretrained(self.pretrained_model, num_labels=output_size)
+        self.model = AutoModelForAudioClassification.from_pretrained(args.pretrained_model, num_labels=output_size)
 
     def forward(self, x):
-        return self.model(x)
+        return self.model(x).logits
     
