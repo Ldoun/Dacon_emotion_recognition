f843b7581980422fae8e273ae1d97b837247f6f5
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-28 16:10:10 +0900

    fix: result path

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     seed_everything(args.seed)
     device = torch.device('cuda:0')
 
-    result_path = os.path.join(args.result_path, str(len(os.listdir(args.result_path))))
+    result_path = os.path.join(args.result_path, args.model+'_'+str(len(os.listdir(args.result_path))))
     os.makedirs(result_path)
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     logger = logging.getLogger()
