605840892ff1865b3b29a57facd2daf99c6b7473
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-05-28 12:48:05 +0900

    fix: use configs per model

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import argparse
 from models import args_for_rnn, args_for_transformer, args_for_HuggingFace
 
 def args_for_data(parser):
+    parser.add_argument('--sr', type=int, default=16000, help='sampling rate')
     parser.add_argument('--train', type=str, default='../data/train.csv')
     parser.add_argument('--test', type=str, default='../data/test.csv')
     parser.add_argument('--submission', type=str, default='../data/sample_submission.csv')
@@ -9,8 +10,6 @@ def args_for_data(parser):
     parser.add_argument('--result_path', type=str, default='./result')
     
 def args_for_audio(parser):
-    parser.add_argument('--sr', type=int, default=16000, help='sampling rate')
-
     #mel-spectrogram
     parser.add_argument('--n_fft', type=int, default=2048, help='length of the windowed signal after padding with zeros')
     parser.add_argument('--win_length', type=int, default=2048, help='each frame of audio is windowed by window of length')
@@ -29,19 +28,20 @@ def args_for_train(parser):
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate for the optimizer')    
 
 def args_for_model(parser, model):
-    if model == "Transformer":
-        args_for_transformer(parser)
-    elif model == "HuggingFace":
+    if model == "HuggingFace":
         args_for_HuggingFace(parser)
     else:
-        args_for_rnn(parser)
+        args_for_audio(parser)
+        if model == "Transformer":
+            args_for_transformer(parser)
+        else:
+            args_for_rnn(parser)
     
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--model', default='', help='name of the model', required=True)
 
-    args_for_audio(parser)
     args_for_data(parser)
     args_for_train(parser)
 
