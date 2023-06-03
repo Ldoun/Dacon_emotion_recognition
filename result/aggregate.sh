cbdb2ce0b8ec6d5217cbdaf27175866739105980
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-06-03 18:55:18 +0900

    fix: path in aggregate.sh

--- result/aggregate.sh
+++ result/aggregate.sh
@@ -6,11 +6,11 @@ total_acc=0
 for k in 1 2 3 4 5 6 7 8 9 10
 do
     echo "${k}-fold" | tr '\n' ': ' >> all.log
-    tail -n $(($2 + 1)) $1_${k}-fold/log.log | head -n 1 >> all.log
-    tail -n $(($2 + 1)) $1_${k}-fold/log.log | head -n 1 | grep -o "[[:digit:]]\.[[:digit:]][[:digit:]][[:digit:]]" | tail -n 2 | tr '\n' ' ' | read loss acc
+    tail -n $(($2 + 1)) $1/${k}-fold/log.log | head -n 1 >> $1/all.log
+    tail -n $(($2 + 1)) $1/${k}-fold/log.log | head -n 1 | grep -o "[[:digit:]]\.[[:digit:]][[:digit:]][[:digit:]]" | tail -n 2 | tr '\n' ' ' | read loss acc
     total_loss=$(($total_loss + $loss))
     total_acc=$(($total_acc + $acc))
 done
 total_loss=$(($total_loss/10))
 total_acc=$(($total_acc/10))
-echo "total loss: ${total_loss} total_acc: ${total_acc}" >> all.log
+echo "total loss: ${total_loss} total_acc: ${total_acc}" >> $1/all.log
